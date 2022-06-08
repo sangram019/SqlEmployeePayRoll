d20d871ce3863a00061afdf78a2a65bdea751f2b
Sangram <sangram15.behera@gmail.com>
2022-06-09 03:24:34 +0530

    Added By Sangram

--- SqlEmployeePayRoll.sql
+++ SqlEmployeePayRoll.sql
@@ -1,2 +1,12 @@
 CREATE DATABASE payroll_service
 use payroll_service;
+
+UC2- Create Employee payroll table
+
+CREATE TABLE employee_payroll 
+(
+id INT IDENTITY(1,1) PRIMARY KEY ,
+name VARCHAR (150),
+salary INT,
+startdate DATE,
+);
