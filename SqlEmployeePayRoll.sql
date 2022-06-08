d272f53069d64ead4cf1a09f1c7d8542433f52c0
Sangram <sangram15.behera@gmail.com>
2022-06-09 03:36:41 +0530

    Added By Sangram

--- SqlEmployeePayRoll.sql
+++ SqlEmployeePayRoll.sql
@@ -14,3 +14,7 @@ startdate DATE,
 UC3-create employee_payroll data
 
 insert into employee_payroll values('Sangram',25000,'2018-03-21'),('Dinesh',20000,'2019-05-27'),('Rakesh',65000,'2010-04-20'),('susant',75000,'2008-06-29'),('Prajna',100000,'2018-07-20');
+
+UC4 - Retrieve all the employee_payroll data
+
+SELECT * FROM employee_payroll;
